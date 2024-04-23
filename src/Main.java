0bfe3ea0a523a57d4de8f4bf9c509774abc014e7
Eduardo Lopez <eddie56.el@gmail.com>
2024-04-23 13:51:42 -0700

    Get 'displayTripSchedule' working

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,7 @@
-import com.mysql.cj.protocol.Resultset;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
-import javax.xml.transform.Result;
 import java.io.FileReader;
 import java.io.IOException;
 import java.sql.*;
@@ -18,17 +16,19 @@ public class Main {
         String username = (String) dbObject.get("username");
         String password = (String) dbObject.get("password");
 
+        String startLocation = "New York City";
+        String destination = "Los Angeles";
+        Date date = new Date(2024 - 1900, 4-1,22);
+
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             Connection con = DriverManager.getConnection(url, username, password);
 
             Statement st = con.createStatement();
 
-            ResultSet rs = st.executeQuery("SELECT * FROM Trip");
+            PomonaTransitSystem pts = new PomonaTransitSystem();
 
-            while(rs.next()){
-                System.out.println(rs.getString("DestinationName"));
-            }
+            pts.displayTripSchedule(con, startLocation, destination, date);
 
             st.close();
             con.close();
--- src/PomonaTransitSystem.java
+++ src/PomonaTransitSystem.java
@@ -7,17 +7,22 @@ public class PomonaTransitSystem {
             String destination,
             Date date
     ) throws SQLException {
-        String query = "SELECT TO.*, TO.ScheduledStartTime, TO.ScheduledArrivalTime, TO.DriverName, TO.BusID " +
-                "FROM TripOffering TO " +
-                "JOIN Trip T ON TO.TripNumber = T.TripNumber " +
-                "WHERE T.StartLocationName = ? AND T.DestinationName = ? AND TO.Date = ?";
+        String query = "SELECT TOF.ScheduledStartTime, TOF.ScheduledArrivalTime, TOF.DriverName, TOF.BusID " +
+                "FROM TripOffering TOF " +
+                "JOIN Trip T ON TOF.TripNumber = T.TripNumber " +
+                "WHERE T.StartLocationName = ? AND T.DestinationName = ? AND TOF.Date = ?";
 
         try(PreparedStatement statement = con.prepareStatement(query)){
             statement.setString(1, startLocation);
             statement.setString(2, destination);
             statement.setString(3, String.valueOf(date));
             try(ResultSet resultSet = statement.executeQuery()){
-                // Process result set and display stops
+                while(resultSet.next()){
+                    System.out.println("Scheduled Start Time: " + resultSet.getString("ScheduledStartTime"));
+                    System.out.println("Scheduled Arrival Time: " + resultSet.getString("ScheduledArrivalTime"));
+                    System.out.println("Driver Name: " + resultSet.getString("DriverName"));
+                    System.out.println("Bus ID: " + resultSet.getString("BusID") + "\n");
+                }
             }
         }
     }
