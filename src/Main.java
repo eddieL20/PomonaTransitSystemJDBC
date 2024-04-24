25f4324dc1e5538507471093096a55cf94fe8471
Eduardo Lopez <eddie56.el@gmail.com>
2024-04-23 21:31:17 -0700

    Finish 'deleteTripOffering' method

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,8 @@ import org.json.simple.parser.ParseException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.sql.*;
+import java.text.SimpleDateFormat;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws SQLException, IOException, ParseException {
@@ -16,24 +18,101 @@ public class Main {
         String username = (String) dbObject.get("username");
         String password = (String) dbObject.get("password");
 
-        String startLocation = "New York City";
-        String destination = "Los Angeles";
-        Date date = new Date(2024 - 1900, 4-1,22);
+        int choice = -1;
+        char resume = 'Y';
+        Date date;
+        Time scheduledStartTime = null;
+        String date_str;
+        String time_str;
+        Scanner scanner = new Scanner(System.in);
 
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             Connection con = DriverManager.getConnection(url, username, password);
-
             Statement st = con.createStatement();
 
             PomonaTransitSystem pts = new PomonaTransitSystem();
 
-            pts.displayTripSchedule(con, startLocation, destination, date);
+            do {
+                System.out.println("1: Display Trip Schedule");
+                System.out.println("2: Delete Trip Offering");
+                System.out.println("3: Add Trip Offering");
+                System.out.println("4: Change Driver for Trip Offering");
+                System.out.println("5: Change Bus for Trip Offering");
+                System.out.println("6: Display Trip Stops");
+                System.out.println("7: Display Weekly Schedule");
+                System.out.println("8: Add Driver");
+                System.out.println("0: Quit");
+
+                if(scanner.hasNextInt()){
+                    choice = scanner.nextInt();
+                    scanner.nextLine();
+
+                    switch(choice){
+                        case 0:
+                            resume = 'N';
+                            break;
+                        case 1:
+                            System.out.print("\nEnter Start Location: ");
+                            String startLocation = scanner.nextLine();
+                            System.out.print("Enter Destination: ");
+                            String destination = scanner.nextLine();
+                            System.out.print("Enter Date (yyyy-mm-dd): ");
+                            date_str = scanner.next();
+                            date = Date.valueOf(date_str);
+
+                            pts.displayTripSchedule(
+                                    con,
+                                    startLocation,
+                                    destination,
+                                    date
+                            );
+
+                            System.out.print("\nContinue? (Y)Yes, (N)Any other key: ");
+                            resume = scanner.next().charAt(0);
+                            String r = String.valueOf(resume).toUpperCase();
+                            resume = r.charAt(0);
+                            break;
+                        case 2:
+                            System.out.print("\nEnter Trip Number: ");
+                            int tripNumber = scanner.nextInt();
+                            scanner.nextLine();
+                            System.out.print("Enter Date (yyyy-mm-dd): ");
+
+                            date_str = scanner.nextLine();
+                            date = Date.valueOf(date_str);
+
+                            System.out.print("Enter Scheduled Start Time: ");
+                            time_str = scanner.nextLine();
+                            SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+                            try{
+                                java.util.Date date1 = sdf.parse(time_str);
+                                scheduledStartTime = new Time(date1.getTime());
+                            } catch (java.text.ParseException e){
+                                System.out.println("Error parsing time: " + e.getMessage());
+                            }
+
+                            pts.deleteTripOffering(
+                                    con,
+                                    tripNumber,
+                                    date,
+                                    scheduledStartTime
+                            );
+                        default:
+                            break;
+                    }
+                } else {
+                    System.out.println("Wrong input, try again...\n");
+                    scanner.nextLine();
+                }
+            } while(resume == 'Y');
+
+            System.out.println("Goodbye...");
 
             st.close();
             con.close();
 
-        } catch (ClassNotFoundException e) {
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
--- src/PomonaTransitSystem.java
+++ src/PomonaTransitSystem.java
@@ -1,4 +1,5 @@
 import java.sql.*;
+import java.util.Scanner;
 
 public class PomonaTransitSystem {
     public void displayTripSchedule(
@@ -7,23 +8,69 @@ public class PomonaTransitSystem {
             String destination,
             Date date
     ) throws SQLException {
-        String query = "SELECT TOF.ScheduledStartTime, TOF.ScheduledArrivalTime, TOF.DriverName, TOF.BusID " +
-                "FROM TripOffering TOF " +
-                "JOIN Trip T ON TOF.TripNumber = T.TripNumber " +
-                "WHERE T.StartLocationName = ? AND T.DestinationName = ? AND TOF.Date = ?";
+
+
+        String query = "SELECT TF.TripNumber, TF.ScheduledStartTime, TF.ScheduledArrivalTime, TF.DriverName, TF.BusID " +
+                "FROM TripOffering TF " +
+                "JOIN Trip T ON TF.TripNumber = T.TripNumber " +
+                "WHERE T.StartLocationName = ? AND T.DestinationName = ? AND TF.Date = ?";
 
         try(PreparedStatement statement = con.prepareStatement(query)){
             statement.setString(1, startLocation);
             statement.setString(2, destination);
-            statement.setString(3, String.valueOf(date));
+            statement.setDate(3, date);
             try(ResultSet resultSet = statement.executeQuery()){
                 while(resultSet.next()){
+                    System.out.println("\n====================================");
+                    System.out.println("Trip Number: " + resultSet.getString("TripNumber"));
                     System.out.println("Scheduled Start Time: " + resultSet.getString("ScheduledStartTime"));
                     System.out.println("Scheduled Arrival Time: " + resultSet.getString("ScheduledArrivalTime"));
                     System.out.println("Driver Name: " + resultSet.getString("DriverName"));
-                    System.out.println("Bus ID: " + resultSet.getString("BusID") + "\n");
+                    System.out.println("Bus ID: " + resultSet.getString("BusID"));
+                    System.out.println("====================================");
                 }
             }
+        }
+    }
+
+    // Delete Trip Offering
+    public void deleteTripOffering(
+            Connection connection,
+            int tripNumber,
+            Date date,
+            Time scheduledStartTime
+    ) throws SQLException {
+        String query = "DELETE FROM TripOffering WHERE TripNumber = ? AND Date = ? AND ScheduledStartTime = ?";
+        try (PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setInt(1, tripNumber);
+            statement.setDate(2, date);
+            statement.setTime(3, scheduledStartTime);
+
+            statement.executeUpdate();
+        }
+    }
+
+    public void addTripOffering(
+            Connection connection,
+            int tripNumber,
+            Date date,
+            Time scheduledStartTime,
+            Time scheduledArrivalTime,
+            String driverName,
+            int busID
+    ) throws SQLException {
+        String query = "INSERT INTO TripOffering (TripNumber, Date, ScheduledStartTime, ScheduledArrivalTime, DriverName, BusID) " +
+                "VALUES (?, ?, ?, ?, ?, ?)";
+
+        try (PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setInt(1, tripNumber);
+            statement.setDate(2, date);
+            statement.setTime(3, scheduledStartTime);
+            statement.setTime(4, scheduledArrivalTime);
+            statement.setString(5, driverName);
+            statement.setInt(6, busID);
+
+            statement.executeUpdate();
         }
     }
 }
